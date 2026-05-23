2345ae3645deb206c229a326cf8b64efac4c3fb8
SentientCorn <radityanarotama0@gmail.com>
2026-05-24 02:45:22 +0700

    feat: adds analytics user fetches

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,13 @@
 import express from "express";
-import { me } from "../controllers/user.js";
+import {
+  me,
+  getUser,
+  getOwnUser,
+  getOwnLatestAnalysis,
+} from "../controllers/user.js";
 
 export default (router: express.Router) => {
   router.get("/user/me", me);
+  router.get("/user/own", getOwnUser);
+  router.get("/user/own/latest", getOwnLatestAnalysis);
 };
